20d4c77c6d3afa54bc67c6daaf518ea49ec40821
MarkoKolarski <marko.kolarski.02@gmail.com>
2025-09-29 00:45:37 +0200

    add initial models

--- back/onlybuns/app/urls.py
+++ back/onlybuns/app/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import LoginView, RegistrationView, activate_account, index
+from .views import LoginView, index
 
 urlpatterns = [
     path('', index, name='index'),
-    path('register/', RegistrationView.as_view(), name='register'),
-    path('activate/<str:activation_token>/', activate_account, name='activate-account'),
     path('login/', LoginView.as_view(), name='login'),
 ]
--- back/onlybuns/app/views.py
+++ back/onlybuns/app/views.py
@@ -1,122 +1,16 @@
 from django.template.loader import render_to_string
 from django.http import HttpResponse
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from rest_framework.decorators import api_view, permission_classes
-from .models import VerifiedUser
-from .serializers import RegistrationSerializer
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
-from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.utils import timezone
 
 def index(request):
     html = render_to_string("index.js", {})
     return HttpResponse(html)
-
-    
-class RegistrationView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        serializer = RegistrationSerializer(data=request.data)
-        
-        if serializer.is_valid():
-            # Kreiramo korisnika pomoću serializer-a
-            serializer.save()
-            return Response({
-                'message': 'The user is successfully registered. An activation link has been sent to your email.'
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET'])
-@permission_classes([AllowAny]) 
-def activate_account(request, activation_token):
-    try:
-        # Validiramo aktivacioni token
-        token = AccessToken(activation_token)
-        
-        # Provera isteka tokena
-        try:
-            token.verify()
-        except TokenError:
-            return HttpResponse(
-                """
-                <html>
-                    <head>
-                        <meta charset="UTF-8">
-                        <title>Account activation</title>
-                    </head>
-                    <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
-                        <h1>Account activation</h1>
-                        <p>The activation link has expired! Please try to register again.</p>
-                    </body>
-                </html>
-                """, content_type="text/html; charset=UTF-8"
-            )
-
-        user_id = token['user_id']
-        user = get_user_model().objects.get(id=user_id)
-
-        # Ako je korisnik već aktiviran, vraćamo odgovarajuću poruku
-        if user.user_type == 'authenticated':
-            return HttpResponse(
-                """
-                <html>
-                    <head>
-                        <meta charset="UTF-8">
-                        <title>Account activation</title>
-                    </head>
-                    <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
-                        <h1>Account activation</h1>
-                        <p>Account already activated! You can sign up now.</p>
-                    </body>
-                </html>
-                """, content_type="text/html; charset=UTF-8"
-            )
-
-        # Aktiviramo korisnika
-        user.user_type = 'authenticated'
-        user.is_active = True
-        user.save()
-
-        VerifiedUser.objects.create(user=user)
-
-        return HttpResponse(
-                """
-                <html>
-                    <head>
-                        <meta charset="UTF-8">
-                        <title>Account activation</title>
-                    </head>
-                    <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
-                        <h1>Account activation</h1>
-                        <p>Account successfully activated! You can sign up now.</p>
-                    </body>
-                </html>
-                """, content_type="text/html; charset=UTF-8"
-            )
-
-    except Exception as e:
-        # Ako token nije validan ili je istekao
-        return HttpResponse(
-                """
-                <html>
-                    <head>
-                        <meta charset="UTF-8">
-                        <title>Account activation</title>
-                    </head>
-                    <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
-                        <h1>Account activation</h1>
-                        <p>The activation link has expired! Please try to register again.</p>
-                    </body>
-                </html>
-                """, content_type="text/html; charset=UTF-8"
-            )
     
 class LoginView(TokenObtainPairView):
     permission_classes = [AllowAny]
