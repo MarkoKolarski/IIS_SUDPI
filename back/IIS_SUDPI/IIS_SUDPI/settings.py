24d1bdb707651dc869108e4fbf5c369d7b3cfb19
MihailoVukorep <mihailo.vukorep@gmail.com>
2025-09-29 16:14:27 +0200

    removed unused settings

--- back/IIS_SUDPI/IIS_SUDPI/settings.py
+++ back/IIS_SUDPI/IIS_SUDPI/settings.py
@@ -98,9 +98,9 @@ DATABASES = {
         'ENGINE': os.getenv('DATABASE_ENGINE'),
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST', ''),
-        'PORT': os.getenv('DATABASE_PORT', ''),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD')
+        #'HOST': os.getenv('DATABASE_HOST', ''),
+        #'PORT': os.getenv('DATABASE_PORT', ''),
     }
 }
 
